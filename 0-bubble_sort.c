66f8a008431ce862a0f012dc5cee5f76503e7f93
Toyin Obafemi <byhisgraceabba@gmail.com>
2023-09-21 16:02:45 +0000

    bubble sort function

---
+++ 0-bubble_sort.c
@@ -0,0 +1,40 @@
+#include "sort.h"
+/**
+ * bubble_sort - bubble sort function
+ * @array: array of integers
+ * @size: size of array
+*/
+
+void bubble_sort(int *array, size_t size)
+{
+	size_t i, j, k;
+
+	for (i = 0; i < size - 1; i++)
+	{
+		for (j = 0; j < size - 1; j++)
+		{
+			if (array[j] > array[j + 1])
+			{
+				k = j + 1;
+				sort(array, j, k);
+				print_array(array, size);
+			}
+		}
+	}
+}
+
+/**
+ * sort - sort function
+ * @array: array
+ * @x: index x
+ * @y: index y
+*/
+
+void sort(int *array, int x, int y)
+{
+	int temp;
+
+	temp = array[x];
+	array[x] = array[y];
+	array[y] = temp;
+}
